a528f761c7f7aa418af2205be2e913ace2bbbb10
CrispyChillies <pnhlong22@clc.fitus.edu.vn>
2026-02-04 22:41:18 +0700

    print Resnet50

--- explanations.py
+++ explanations.py
@@ -667,6 +667,7 @@ class SimCAM(nn.Module):
 
         self.extractor = ModelOutputs(
             self.model, self.feature_module, target_layers, return_gradients=False)
+        print(self.extractor)
 
     def Point_Specific(self, decom, point=[0, 0], size=(224, 224)):
         """
